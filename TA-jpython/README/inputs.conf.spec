7b820d12af2daab14679975a01266b3e36392022
anabasis <anabasis01@gmail.com>
2018-08-28 03:58:41 +0900

    SPLUNK INIT

---
+++ TA-jpython/README/inputs.conf.spec
@@ -0,0 +1,8 @@
+[input_param1://<name>]
+text = 
+password = 
+checkbox = 
+dropdown_list = 
+multiple_dropdown = 
+radio_buttons = 
+global_account =
---
+++ TA-jpython/bin/input_module_input_param1.py
@@ -0,0 +1,142 @@
+
+# encoding = utf-8
+
+import os
+import sys
+import time
+import datetime
+
+'''
+    IMPORTANT
+    Edit only the validate_input and collect_events functions.
+    Do not edit any other part in this file.
+    This file is generated only once when creating the modular input.
+'''
+'''
+# For advanced users, if you want to create single instance mod input, uncomment this method.
+def use_single_instance_mode():
+    return True
+'''
+
+def validate_input(helper, definition):
+    """Implement your own validation logic to validate the input stanza configurations"""
+    # This example accesses the modular input variable
+    # text = definition.parameters.get('text', None)
+    # password = definition.parameters.get('password', None)
+    # checkbox = definition.parameters.get('checkbox', None)
+    # dropdown_list = definition.parameters.get('dropdown_list', None)
+    # multiple_dropdown = definition.parameters.get('multiple_dropdown', None)
+    # radio_buttons = definition.parameters.get('radio_buttons', None)
+    # global_account = definition.parameters.get('global_account', None)
+    pass
+
+def collect_events(helper, ew):
+    """Implement your data collection logic here
+
+    # The following examples get the arguments of this input.
+    # Note, for single instance mod input, args will be returned as a dict.
+    # For multi instance mod input, args will be returned as a single value.
+    opt_text = helper.get_arg('text')
+    opt_password = helper.get_arg('password')
+    opt_checkbox = helper.get_arg('checkbox')
+    opt_dropdown_list = helper.get_arg('dropdown_list')
+    opt_multiple_dropdown = helper.get_arg('multiple_dropdown')
+    opt_radio_buttons = helper.get_arg('radio_buttons')
+    opt_global_account = helper.get_arg('global_account')
+    # In single instance mode, to get arguments of a particular input, use
+    opt_text = helper.get_arg('text', stanza_name)
+    opt_password = helper.get_arg('password', stanza_name)
+    opt_checkbox = helper.get_arg('checkbox', stanza_name)
+    opt_dropdown_list = helper.get_arg('dropdown_list', stanza_name)
+    opt_multiple_dropdown = helper.get_arg('multiple_dropdown', stanza_name)
+    opt_radio_buttons = helper.get_arg('radio_buttons', stanza_name)
+    opt_global_account = helper.get_arg('global_account', stanza_name)
+
+    # get input type
+    helper.get_input_type()
+
+    # The following examples get input stanzas.
+    # get all detailed input stanzas
+    helper.get_input_stanza()
+    # get specific input stanza with stanza name
+    helper.get_input_stanza(stanza_name)
+    # get all stanza names
+    helper.get_input_stanza_names()
+
+    # The following examples get options from setup page configuration.
+    # get the loglevel from the setup page
+    loglevel = helper.get_log_level()
+    # get proxy setting configuration
+    proxy_settings = helper.get_proxy()
+    # get account credentials as dictionary
+    account = helper.get_user_credential_by_username("username")
+    account = helper.get_user_credential_by_id("account id")
+    # get global variable configuration
+    global_text = helper.get_global_setting("text")
+    global_password = helper.get_global_setting("password")
+    global_checkbox = helper.get_global_setting("checkbox")
+
+    # The following examples show usage of logging related helper functions.
+    # write to the log for this modular input using configured global log level or INFO as default
+    helper.log("log message")
+    # write to the log using specified log level
+    helper.log_debug("log message")
+    helper.log_info("log message")
+    helper.log_warning("log message")
+    helper.log_error("log message")
+    helper.log_critical("log message")
+    # set the log level for this modular input
+    # (log_level can be "debug", "info", "warning", "error" or "critical", case insensitive)
+    helper.set_log_level(log_level)
+
+    # The following examples send rest requests to some endpoint.
+    response = helper.send_http_request(url, method, parameters=None, payload=None,
+                                        headers=None, cookies=None, verify=True, cert=None,
+                                        timeout=None, use_proxy=True)
+    # get the response headers
+    r_headers = response.headers
+    # get the response body as text
+    r_text = response.text
+    # get response body as json. If the body text is not a json string, raise a ValueError
+    r_json = response.json()
+    # get response cookies
+    r_cookies = response.cookies
+    # get redirect history
+    historical_responses = response.history
+    # get response status code
+    r_status = response.status_code
+    # check the response status, if the status is not sucessful, raise requests.HTTPError
+    response.raise_for_status()
+
+    # The following examples show usage of check pointing related helper functions.
+    # save checkpoint
+    helper.save_check_point(key, state)
+    # delete checkpoint
+    helper.delete_check_point(key)
+    # get checkpoint
+    state = helper.get_check_point(key)
+
+    # To create a splunk event
+    helper.new_event(data, time=None, host=None, index=None, source=None, sourcetype=None, done=True, unbroken=True)
+    """
+
+    '''
+    # The following example writes a random number as an event. (Multi Instance Mode)
+    # Use this code template by default.
+    import random
+    data = str(random.randint(0,100))
+    event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=data)
+    ew.write_event(event)
+    '''
+
+    '''
+    # The following example writes a random number as an event for each input config. (Single Instance Mode)
+    # For advanced users, if you want to create single instance mod input, please use this code template.
+    # Also, you need to uncomment use_single_instance_mode() above.
+    import random
+    input_type = helper.get_input_type()
+    for stanza_name in helper.get_input_stanza_names():
+        data = str(random.randint(0,100))
+        event = helper.new_event(source=input_type, index=helper.get_output_index(stanza_name), sourcetype=helper.get_sourcetype(stanza_name), data=data)
+        ew.write_event(event)
+    '''
